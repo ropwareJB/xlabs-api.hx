b45a2152acd8481f7e20e6a7ca5dc5e537f69402
Josh Brown <josh@epidev.com>
2015-03-14 22:12:05 +1100

    Little bitta commenting

--- src/xlabs/Xlabs.hx
+++ src/xlabs/Xlabs.hx
@@ -27,6 +27,12 @@ class Xlabs{
     untyped window.xLabs = new Xlabs();
   }
 
+  /*
+   * Constructor
+   *    Create a new instance of the API interface object, and attach listeners
+   *    for when the API has entered its ready state, when the dataset is updated,
+   *    and when the IdPath is set.
+   */
   public function new(){
     var doc = Browser.document;
     doc.addEventListener("xLabsApiReady", onApiReady);
@@ -34,6 +40,10 @@ class Xlabs{
     doc.addEventListener("xLabsApiIdPath", function(event){onApiIdPath(event.detail);});
   }
 
+  /*
+   * setConfig
+   *    Send a query to the xlabs plugin for some data or to perform some function
+   */
   public function setConfig(path:String, value):Void{
     var config = {path: path, value:value};
     var message = {target: 'xLabs', config: config};
@@ -57,6 +67,8 @@ class Xlabs{
   ////////////////////////////////////////////////
   // Calibration
   ////////////////////////////////////////////////
+
+
   inline private function getTimestamp():Float{
     return Date.now().getTime();
   }
@@ -65,6 +77,11 @@ class Xlabs{
     t1 = 0;
   }
 
+  /*
+   * updateCalibrationTruth
+   *    Add a data point to the calibration to decrease the uncertaintly
+   *    factor and hence increase accuracy
+   */
   public function updateCalibrationTruth(xScreen, yScreen):Void{
     var t1:Float = this.t1;
     var t2:Float = getTimestamp();
@@ -78,6 +95,11 @@ class Xlabs{
     this.t1 = t2;
   }
 
+  /*
+   * addCalibrationTruth
+   *    Add a data point during the calibration procedure, recording
+   *    the looked-at point and the screen point.
+   */
   public function addCalibrationTruth(t1:Float, t2:Float, xScreen, yScreen):Void{
     var csv = t1+","+t2+","+xScreen+","+yScreen;
 #if debug
@@ -86,6 +108,11 @@ class Xlabs{
     setConfig("truth.append", csv);
   }
 
+  /*
+   * calibrate 
+   *    Request initiation of the calibration procedure on the xlabs
+   *    browser plugin. Used in conjunction with the calibrationClear method
+   */
   public function calibrate(?id="3p"):Void{
     setConfig("calibration.request", id);
 #if debug
@@ -93,6 +120,10 @@ class Xlabs{
 #end
   }
 
+  /*
+   * calibrationClear
+   *    Clear the calibration, to force recalibration at a later time
+   */
   public function calibrationClear():Void{
     setConfig("calibration.clear", null);
 #if debug
@@ -118,7 +149,7 @@ class Xlabs{
 
   /* Calculate PPI/DPI
    * Source: http://dpi.lv/ */
-  private function calcDpi(?w:Float=1, ?h:Float=1, ?d:Float=13.3, ?opt:String='d'):Float{
+  inline private function calcDpi(?w:Float=1, ?h:Float=1, ?d:Float=13.3, ?opt:String='d'):Float{
     var dpi = (opt == 'd' ? Math.sqrt(w*w + h*h) : opt == 'w' ? w : h) / d;
     return dpi>0 ? Math.round(dpi) : 0;
   }
@@ -127,6 +158,11 @@ class Xlabs{
   // Coordinate conversion
   //////////////////////////////////////////////
 
+  /*
+   *  documentOffset
+   *    Get the offset of the document as a Point object.
+   *    Probably better described as the scroll
+   */
   public function documentOffset():Point{
     if(!documentOffsetReady()) throw new Error(DOC_OFFSET_ERROR_MSG);
 
@@ -135,10 +171,19 @@ class Xlabs{
     return {x:x, y:y};
   }
 
+  /*
+   * documentOffsetReady
+   *    Check if the document offset data is readily-available
+   */
   public function documentOffsetReady():Bool{
     return getConfig("browser.document.offset.ready") == "1";
   }
 
+  /*
+   * Screen x-Point to Document x-Point
+   *    Convert a point from the screen's reference to the
+   *    a point from the document's point of reference.     
+   */
   private function scr2docX(screenX:Float):Float{
     if(!documentOffsetReady()) throw new Error(DOC_OFFSET_ERROR_MSG);
 
@@ -146,6 +191,11 @@ class Xlabs{
     return screenX - Browser.window.screenX - xOffset;
   }
 
+  /*
+   * Screen y-Point to Document y-Point
+   *    Convert a point from the screen's reference to the
+   *    a point from the document's point of reference.
+   * */
   private function scr2docY(screenY:Float):Float{
     if(!documentOffsetReady()) throw new Error(DOC_OFFSET_ERROR_MSG);
 
@@ -153,6 +203,11 @@ class Xlabs{
     return screenY - Browser.window.screenY - yOffset;
   }
 
+  /*
+   * Screen Point to Document Point
+   *    Convert a point from the screen's reference to the
+   *    a point from the document's point of reference.
+   * */
   public function scr2doc(screenX:Float, screenY:Float):Point{
     return {
       x: scr2docX(screenX),
@@ -160,6 +215,11 @@ class Xlabs{
     };
   }
 
+  /*
+   * Document x-Point to screen x-Point
+   *    Convert a point from the document's reference to the
+   *    a point from the screen's point of reference.
+   * */
   private function doc2scrX(docX:Float):Float{
     if(!documentOffsetReady()) throw new Error(DOC_OFFSET_ERROR_MSG);
 
@@ -167,6 +227,11 @@ class Xlabs{
     return docX + Browser.window.screenX + xOffset;
   }
 
+  /*
+   * Document y-Point to screen y-Point
+   *    Convert a point from the document's reference to the
+   *    a point from the screen's point of reference.
+   * */
   private function doc2scrY(docY:Float):Float{
     if(!documentOffsetReady()) throw new Error(DOC_OFFSET_ERROR_MSG);
 
@@ -174,6 +239,11 @@ class Xlabs{
     return docY + Browser.window.screenY + yOffset;
   }
 
+  /*
+   * Document Point to screen Point
+   *    Convert a point from the document's reference to the
+   *    a point from the screen's point of reference.
+   * */
   public function doc2scr(docX:Float, docY:Float):Point{
     return {
       x: doc2scrX(docX),
@@ -185,21 +255,41 @@ class Xlabs{
   /////////////////////////////////////////////////////
   // Setup
   /////////////////////////////////////////////////////
-
-  public function onApiReady():Void{
+  /*
+   *  onApiReady
+   *    Listener for the xlabs ApiReady event,
+   *    notifies its listener when the event occurs.
+   */
+  private function onApiReady():Void{
     apiReady = true;
     if(callbackReady != null) callbackReady();
   }
 
-  public function onApiState(config):Void{
+  /*
+   * onApiState 
+   *    Listener for the xlabs onApiState event,
+   *    notifies its listener when the event occurs,
+   *    and updates the reported data set.
+   */
+  private function onApiState(config):Void{
     this.config = config;
     if(callbackState != null) callbackState();
   }
 
-  public function onApiIdPath(detail):Void{
+  /*
+   * onApiIdPath 
+   *    Listener for the xlabs onApiIdPath event,
+   *    notifies its listener when the event occurs.
+   */
+  private function onApiIdPath(detail):Void{
     if(callbackIdPath != null) callbackIdPath(detail.id, detail.path);
   }
 
+  /*
+   *  setup 
+   *    Listener for the xlabs ApiReady event,
+   *    notifies its listener when the event occurs.
+   */
   public function setup(?callbackReady:Void->Void, ?callbackState:Void->Void, ?callbackIdPath:String->String->Void){
     this.callbackReady = callbackReady;
     this.callbackState = callbackState;
